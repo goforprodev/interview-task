d5a491d76b90584d66d733d762a75a6c71f32577
Promise <goforprodev@gmail.com>
2023-06-23 12:42:26 +0100

    Add files via upload

---
+++ interview/index.js
@@ -0,0 +1,7 @@
+/*
+Front end dev. task:
+Using HTML, CSS, and JavaScript, create a user-friendly form that includes validation for the following requirements:
+Name: Should not be empty and should contain only letters.
+Email address: Should be a valid email format (e.g., "example@example.com").
+Password: Should have a minimum length of 8 characters and contain at least one uppercase letter, one lowercase letter, and one numeric digit
+*/
